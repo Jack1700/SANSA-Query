8b64d9f1313cf6f8244df43bbb0ef3415fed6998
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2019-05-02 13:11:49 +0200

    Story 2 nearly done

--- sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/tablewise/Sparql2SqlTablewise.scala
+++ sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/tablewise/Sparql2SqlTablewise.scala
@@ -13,11 +13,11 @@ import java.util.List
 
 class Sparql2SqlTablewise {
 
-  def for1Pattern(subject: String, predicate: String, _object: String): String = {
+  def for1Pattern(subject: String, predicate: String, _object: String, tableNum: Int): String = {
 
     var beforeWhere = false;
     var select = "SELECT ";
-    val from = " FROM Triples ";
+    val from = "FROM Triples ";
     var where = "WHERE "
 
     select += "Triples.Subject "
@@ -30,11 +30,9 @@ class Sparql2SqlTablewise {
     }
     select += ", Triples.Predicate"
     if (predicate(0) == '"') {
-      if (beforeWhere) {
-        where += " And "
-      }
       where += "Triples.Predicate=" + predicate
-
+      if (beforeWhere)
+        where += " And "
       beforeWhere = true
     } else {
       select += " AS " + predicate + " "
@@ -43,15 +41,13 @@ class Sparql2SqlTablewise {
     select += ", Triples.Object"
 
     if (_object(0) == '"') {
-      if (beforeWhere) {
-        where += " And "
-      }
       where += "Triples.Object=" + _object;
-
+      if (beforeWhere)
+        where += " And"
     } else {
       select += " AS " + _object + " "
     }
-    return "(" + select + from + where + ")"
+    return "(" + select + from + where + ")" + " AS " + tableNum + " "
   }
 
   def cleanProjectVariables(projectVariables: List[Var]): String = {
@@ -63,7 +59,65 @@ class Sparql2SqlTablewise {
     return variables.toString.substring(12, variables.toString.size - 1);
   }
 
-  def SQLBuilder(tableName: String, QueryString: String): String = {
+  def joinOn(lastSubject: String, lastPredicate: String, lastObject: String, subject: String, predicate: String, _object: String,
+             tableNum1: Int, tableNum2: Int): String = {
+    var joinStatement = " ON "
+    var joined = false
+    if (lastSubject(0) != '"') {
+      lastSubject match {
+        case subject   => joinStatement += tableNum1 + "." + lastSubject + "=" + tableNum2 + "." + subject + " "; joined = true;
+        case predicate => joinStatement += tableNum1 + "." + lastSubject + "=" + tableNum2 + "." + predicate + " "; joined = true;
+        case _object   => joinStatement += tableNum1 + "." + lastSubject + "=" + tableNum2 + "." + _object + " "; joined = true;
+      }
+    }
+
+    if (lastPredicate(0) != '"') {
+      lastPredicate match {
+        case subject => {
+          if (joined)
+            joinStatement += " AND "
+          joined = true;
+          joinStatement += tableNum1 + "." + lastPredicate + "=" + tableNum2 + "." + subject + " ";
+        }
+        case predicate => {
+          if (joined)
+            joinStatement += " AND "
+          joined = true;
+          joinStatement += tableNum1 + "." + lastPredicate + "=" + tableNum2 + "." + predicate + " ";
+        }
+        case _object => {
+          if (joined)
+            joinStatement += " AND "
+          joined = true;
+          joinStatement += tableNum1 + "." + lastPredicate + "=" + tableNum2 + "." + _object + " ";
+        }
+      }
+    }
+    if (lastObject(0) != '"') {
+      lastObject match {
+        case subject => {
+          if (joined)
+            joinStatement += " AND "
+          joinStatement += tableNum1 + "." + lastObject + "=" + tableNum2 + "." + subject + " ";
+        }
+        case predicate => {
+          if (joined)
+            joinStatement += " AND "
+          joinStatement += tableNum1 + "." + lastObject + "=" + tableNum2 + "." + predicate + " ";
+
+        }
+        case _object => {
+          if (joined)
+            joinStatement += " AND "
+          joinStatement += tableNum1 + "." + lastObject + "=" + tableNum2 + "." + _object + " ";
+
+        }
+      }
+    }
+
+    return joinStatement
+  }
+  def Sparql2SqlTablewise(QueryString: String): String = {
     val query = QueryFactory.create(QueryString);
     TripleGetter.generateStringTriples(query);
 
@@ -75,11 +129,16 @@ class Sparql2SqlTablewise {
       val subject = TripleGetter.getSubjects()(i);
       val predicate = TripleGetter.getPredicates()(i);
       val _object = TripleGetter.getObjects()(i);
-      from += for1Pattern(subject, predicate, _object);
-      if (i < TripleGetter.getSubjects().size - 1) {
-        from += "\n" + "JOIN\n";
-        // TO DO : ADD JOIN CONDITION (ON)
+      var addToFrom = ""
+      if (i > 0) {
+        val lastSubject = TripleGetter.getSubjects()(i - 1);
+        val lastPredicate = TripleGetter.getPredicates()(i - 1);
+        val lastObject = TripleGetter.getObjects()(i - 1);
+        from += "\n Join \n";
+        addToFrom = "\n" + joinOn(lastSubject, lastPredicate, lastObject, subject, predicate, _object, i - 1, i) + "\n"
       }
+      from += for1Pattern(subject, predicate, _object, i);
+      from += addToFrom
 
     }
     return select + from;
