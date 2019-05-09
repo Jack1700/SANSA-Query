81df44c46668387b54a9c38def755a5a8795e730
GideonKoenig <ginosiassa.koenig@online.de>
2019-05-09 11:08:24 +0200

    code cleaning

--- sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/IsmailTest.scala
+++ sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/IsmailTest.scala
@@ -1,6 +1,6 @@
 package net.sansa_stack.query.spark.tablewise
-import scala.io.Source
 
+import scala.io.Source
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.scalatest.FunSuite
 import net.sansa_stack.query.spark.tablewise
@@ -18,14 +18,17 @@ class IsmailTest extends FunSuite with DataFrameSuiteBase {
     //create Spark Session
     val OurProgram = new Sparql2SqlTablewise()
     val input = getClass.getResource("/datasets/bsbm-sample.nt").getPath
-   // val query = getClass.getResource("/queries/bsbm/Q1.sparql").getPath
+    //val query = getClass.getResource("/queries/bsbm/Q1.sparql").getPath
     val triples = spark.rdf(Lang.NTRIPLES)(input)
     val df = triples.toDF()
+    
     // query from test resources 
     val query = "src/test/resources/queries/bsbm/Q1.sparql"
     val fileContents = Source.fromFile(query).getLines.mkString
+    
     // our translation 
     val result = OurProgram.createQueryExecution(spark, fileContents)
+    
     // right query gives 7 back 
     // val result = spark.sql("SELECT X FROM (SELECT  triples.s  AS X , triples.p , triples.o  FROM triples  WHERE  triples.p=\"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature\" And  triples.o= \"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature40\")")
     print(result.count())
@@ -44,10 +47,11 @@ class IsmailTest extends FunSuite with DataFrameSuiteBase {
      val query = "src/test/resources/queries/bsbm/Q2.sparql"
     val fileContents = Source.fromFile(query).getLines.mkString
     print(fileContents)
+    
     // our translation 
     val result = OurProgram.createQueryExecution(spark, fileContents)
+    
     // right query gives 4 back 
-    // val result = spark.sql("SELECT X FROM (SELECT  triples.s  AS X , triples.p , triples.o  FROM triples  WHERE  triples.p=\"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature\" And  triples.o= \"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature40\")")
     print(result.count())
     assert(result.count() == 4)
   }
