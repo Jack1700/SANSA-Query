df4db320b5dfe8c3e496101a6139b94d8e5d1354
jakob <jakob.greis@gmx.de>
2019-05-09 10:14:33 +0200

    Update Test to read Query from Resources and execute with tablewise

--- sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/IsmailTest.scala
+++ sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/IsmailTest.scala
@@ -1,4 +1,5 @@
 package net.sansa_stack.query.spark.tablewise
+import scala.io.Source
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.scalatest.FunSuite
@@ -21,16 +22,34 @@ class IsmailTest extends FunSuite with DataFrameSuiteBase {
     val triples = spark.rdf(Lang.NTRIPLES)(input)
     val df = triples.toDF()
     // query from test resources 
-    val Q1 = """SELECT ?X
-    WHERE {
-        ?X <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature40> .
-    }"""
+    val query = "src/test/resources/queries/bsbm/Q1.sparql"
+    val fileContents = Source.fromFile(query).getLines.mkString
     // our translation 
-    val result = OurProgram.createQueryExecution(spark, Q1)
+    val result = OurProgram.createQueryExecution(spark, fileContents)
     // right query gives 7 back 
     // val result = spark.sql("SELECT X FROM (SELECT  triples.s  AS X , triples.p , triples.o  FROM triples  WHERE  triples.p=\"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature\" And  triples.o= \"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature40\")")
     print(result.count())
     assert(result.count() == 7)
+  }
+  
+   test("test 2") {
+    //create Spark Session
+    val OurProgram = new Sparql2SqlTablewise()
+    val input = getClass.getResource("/datasets/bsbm-sample.nt").getPath
+   // val query = getClass.getResource("/queries/bsbm/Q1.sparql").getPath
+    val triples = spark.rdf(Lang.NTRIPLES)(input)
+    val df = triples.toDF()
+    
+    // query from test resources 
+     val query = "src/test/resources/queries/bsbm/Q2.sparql"
+    val fileContents = Source.fromFile(query).getLines.mkString
+    print(fileContents)
+    // our translation 
+    val result = OurProgram.createQueryExecution(spark, fileContents)
+    // right query gives 4 back 
+    // val result = spark.sql("SELECT X FROM (SELECT  triples.s  AS X , triples.p , triples.o  FROM triples  WHERE  triples.p=\"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature\" And  triples.o= \"http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature40\")")
+    print(result.count())
+    assert(result.count() == 4)
   }
   
 
