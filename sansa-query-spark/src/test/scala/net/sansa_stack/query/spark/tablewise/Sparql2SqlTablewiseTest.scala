b862dc7a554f66758fb154d4d8c1ff1a39d7a173
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2019-05-23 11:53:53 +0200

    Test with comparing results done.

--- sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/Sparql2SqlTablewiseTest.scala
+++ sansa-query-spark/src/test/scala/net/sansa_stack/query/spark/tablewise/Sparql2SqlTablewiseTest.scala
@@ -1,80 +1,24 @@
 package net.sansa_stack.query.spark.tablewise
 import scala.io.Source
+import java.io.File
+import java.io.FileOutputStream
+
+import org.apache.jena.query.QueryExecutionFactory
+import org.apache.jena.query.QueryFactory
+import org.apache.jena.query.ResultSetFormatter
+import org.apache.jena.query.Syntax
+import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.riot.Lang
+import org.apache.jena.shared.PrefixMapping
+import org.scalatest.FunSuite
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import org.scalatest.FunSuite
-import net.sansa_stack.query.spark.tablewise
-import org.apache.spark.sql.SparkSession
+
 import net.sansa_stack.rdf.spark.io._
-import net.sansa_stack.rdf.spark.stats._
-import org.apache.jena.riot.Lang
 import net.sansa_stack.rdf.spark.model._
-import net.sansa_stack.rdf.spark.io._
-import org.apache.jena.riot.Lang
-import org.apache.jena.query.{ QueryExecutionFactory, QuerySolutionMap, QueryFactory, ResultSetFormatter, Syntax }
-import org.apache.jena.tdb.TDBFactory
-import org.apache.jena.query.ResultSet
-import org.apache.jena.query.QueryExecution
-import org.apache.jena.shared.PrefixMapping
-import org.apache.jena.rdf.model.ModelFactory
 
 class Sparql2SqlTablewiseTest extends FunSuite with DataFrameSuiteBase {
 
-  test("test 1") {
-    val OurProgram = new Sparql2SqlTablewise()
-    val input = getClass.getResource("/datasets/bsbm-sample.nt").getPath
-    val triples = spark.rdf(Lang.NTRIPLES)(input)
-    val df = triples.toDF()
-
-    // query from test resources
-    val query = "src/test/resources/queries/bsbm/Q1.sparql"
-    val fileContents = Source.fromFile(query).getLines.mkString
-
-    // our translation
-    val result = OurProgram.createQueryExecution(spark, fileContents)
-    print(result.count())
-    assert(result.count() == 7)
-  }
-
-  test("test 2") {
-    val OurProgram = new Sparql2SqlTablewise()
-    val input = getClass.getResource("/datasets/bsbm-sample.nt").getPath
-    val triples = spark.rdf(Lang.NTRIPLES)(input)
-    val df = triples.toDF()
-
-    // query from test resources
-    val query = "src/test/resources/queries/bsbm/Q5.sparql"
-    val fileContents = Source.fromFile(query).getLines.mkString
-
-    // our translation
-    val result = OurProgram.createQueryExecution(spark, fileContents)
-    print(result.count())
-    assert(result.count() == 674)
-  }
-
-  test("test 3") {
-    val OurProgram = new Sparql2SqlTablewise()
-    val input = getClass.getResource("/datasets/bsbm-sample.nt").getPath
-    val triples = spark.rdf(Lang.NTRIPLES)(input)
-    val df = triples.toDF()
-
-    // query from test resources
-    val query = "src/test/resources/queries/bsbm/Q6.sparql"
-    val fileContents = Source.fromFile(query).getLines.mkString
-    
-    val defModel = ModelFactory.createDefaultModel();
-    val mymodel = defModel.read("src/test/resources/datasets/bsbm-sample.nt");
-    val sparqlQuery = QueryFactory.create(fileContents);
-    sparqlQuery.setPrefixMapping(PrefixMapping.Standard);
-    QueryFactory.parse(sparqlQuery, fileContents, "", Syntax.syntaxSPARQL_11);
-    val qexec = QueryExecutionFactory.create(sparqlQuery, mymodel);
-    val r = qexec.execSelect();
-    
-    // our translation
-    val result = OurProgram.createQueryExecution(spark, fileContents)
-    print(result.count())
-    assert(result.count() == r.getRowNumber)
-  }
 
   test("test 4") {
     // THIS TEST EXCUTES THE ACTUAL SPARQL QUERY THEN COMPARE THE RESULTS WITH SQL QUERY. WORKS PERFECTLY
@@ -84,7 +28,7 @@ class Sparql2SqlTablewiseTest extends FunSuite with DataFrameSuiteBase {
     val df = triples.toDF()
 
     // query from test resources
-    val queryPath = "src/test/resources/queries/bsbm/Q7.sparql"
+    val queryPath = "src/test/resources/queries/bsbm/Q6.sparql"
     val fileContents = Source.fromFile(queryPath).getLines.mkString
 
     val defModel = ModelFactory.createDefaultModel();
@@ -94,12 +38,16 @@ class Sparql2SqlTablewiseTest extends FunSuite with DataFrameSuiteBase {
     QueryFactory.parse(sparqlQuery, fileContents, "", Syntax.syntaxSPARQL_11);
     val qexec = QueryExecutionFactory.create(sparqlQuery, mymodel);
     val r = qexec.execSelect();
-    // ResultSetFormatter.outputAsCSV(r); //prints out the result
-    println("this is number of rows with sparql " + r.getRowNumber)
+    val sparqlResultFile = new File("src/test/resources/testresults/results.csv")
+    val outputSteam = new FileOutputStream(sparqlResultFile);
+    ResultSetFormatter.outputAsCSV(outputSteam, r); //prints out the result
+    println("this is number of rows with sparql " + r.getRowNumber())
 
-    // our translation
+    val sparqlDataFrame = spark.read.format("csv").option("header", "true").load("src/test/resources/testresults/results.csv")
 
     val result = OurProgram.createQueryExecution(spark, fileContents)
-    assert(result.count() == r.getRowNumber)
+ 
+    val intersection = result.intersect(sparqlDataFrame)
+    assert(result.count() == sparqlDataFrame.count() && result.count() == intersection.count() )
   }
 }
