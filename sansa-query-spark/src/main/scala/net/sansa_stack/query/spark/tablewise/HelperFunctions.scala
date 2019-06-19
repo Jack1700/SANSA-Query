fd2a5d8ba145b8a7064227e54c0eaaafc6848a0e
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2019-06-19 17:47:46 +0200

    completed mapToTypedDF

--- sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/tablewise/HelperFunctions.scala
+++ sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/tablewise/HelperFunctions.scala
@@ -5,6 +5,8 @@ import org.apache.jena.sparql.core.Var
 import scala.collection.mutable.ArrayBuffer
 import java.util.List
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
 
 class HelperFunctions {
 
@@ -93,9 +95,57 @@ class HelperFunctions {
   def mapToTypedDF(df: DataFrame, columnNames: ArrayBuffer[String], columnTypes: ArrayBuffer[String]): DataFrame = {
 
     import org.apache.spark.sql.functions._
+    var df2 = df
     for (column <- columnNames) {
-      df.select(col(column), substring_index(col(column), "\\^", 1).as(column))
+      val mySchema = df2.schema
+      var c = df.withColumn("_tmp", split(col(column), "\\^")).select(
+        col("_tmp").getItem(0).as(column)).drop("_tmp")
+      val myList = c.select(column).rdd.map(r => r(0)).collect()
+      val myListofStrings = myList.map(str => str.toString);
+      var index = 0
+
+      df2 = df2.drop(column)
+      var rdd = df2.rdd
+
+      var rdd2 = rdd.map((row: Row) => {
+        var rowList = row.toSeq.toList;
+        index += 1;
+        val r = Row.fromSeq(rowList :+ myListofStrings(index))
+        r
+      })
+
+      val spark = SparkSession.builder().getOrCreate()
+      df2 = spark.createDataFrame(rdd2, mySchema)
+
     }
-    df
+
+    // data types:  ("integer", "decimal", "float", "double", "string", "boolean", "dateTime")
+    //CAST
+    var i = 0
+    for (i <- 0 until columnNames.size) {
+      import org.apache.spark.sql.types._
+      var myType:DataType = IntegerType
+      columnTypes(i) match {
+        case "integer"  => myType = IntegerType
+        case "decimal"  => myType = DecimalType(5,2)
+        case "float"    => myType = FloatType
+        case "double"   => myType = DoubleType
+        case "string"   => myType = StringType
+        case "boolean"  => myType = BooleanType
+        case "dateTime" => myType = TimestampType
+      }
+       println("VAAAAAAAALLLLLLUEEEEEEEEEEE IS "+ myType)
+       val df3= df2.selectExpr("*")
+       df2 = df3.withColumn("Tmp", df2.col(columnNames(i)).cast(myType))
+        .drop(columnNames(i))
+        .withColumnRenamed("Tmp", columnNames(i))
+    }
+    
+    //ORDER BY
+    for (i <- 0 until columnNames.size) {
+      val index = columnNames.size - 1 - i
+      df2 = df2.orderBy(columnNames(index))
+    }
+    df2
   }
 }
